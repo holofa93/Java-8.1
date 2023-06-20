463254c3235e92b9f8fc63e3d54ff4e4c9ce8c0d
Neto test <vovka9313@mail.ru>
2023-06-20 20:54:34 +0300

    homework

--- Homework8.1/src/main/java/ru/netology/javaqa8_1/StatisticsService.java
+++ Homework8.1/src/main/java/ru/netology/javaqa8_1/StatisticsService.java
@@ -1,6 +1,6 @@
 package ru.netology.javaqa8_1;
 
-public class StatisticsService
+public class StatisticsService {
         public long findMax(long[] incomes) {
             long currentMax = incomes[0];
             for (long income : incomes) {
--- Homework8.1/src/test/java/StatisticsServiceTest.java
+++ Homework8.1/src/test/java/StatisticsServiceTest.java
@@ -1,5 +1,7 @@
 package ru.netology.javaqa8_1;
+
 import org.junit.jupiter.api.Test;
+
 import org.junit.jupiter.api.Assertions;
 
     public class StatisticsServiceTest {
